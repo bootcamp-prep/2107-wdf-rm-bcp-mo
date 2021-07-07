b458f058c10649053bf5004bc776950a758c4b91
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-07-07 08:20:46 +0530

    bonus

---
+++ bonus-material/extra-concepts.js
@@ -0,0 +1,14 @@
+/* 
+    Concepts below are good to know as they will make your life easier in the long run and as you grow as an engineer. You don't need to know them to pass the Bootcamp interview though. So only spend time learning them if you have time. Will add more to the list throughout the course
+
+    to learn more about each concept, copy the concept and search it using google. Or ask me/Laura about it. 
+    
+    For example, search "Math.min() js mdn" to learn more about Math.min() in javascript
+*/
+
+Math.min();
+Math.max();
+Math.abs();
+Math.sqrt();
+Math.pow();
+Math.random();
